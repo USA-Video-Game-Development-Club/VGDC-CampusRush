138f52d7e463785626798f5212b6eae595167950
Farstrider3121 <farstrider3121@gmail.com>
2023-04-11 09:25:24 -0500

    Score tracking functionality

---
+++ CampusRush/Assets/Scripts/UI/ScoreTracker.cs
@@ -0,0 +1,28 @@
+//Author: Rhiannon Strickland
+//Date: April 7, 2023
+//This script controls the score tracking. Just call the score tracker and it will update the rest!
+using UnityEngine;
+using TMPro;
+
+public class ScoreTracker : MonoBehaviour
+{
+    [SerializeField]
+    TextMeshProUGUI text;
+    public int score = 0;
+
+    private void Awake()
+    {
+        text.text = "Score: " + score;
+    }
+
+    public void AddPoints(int points)
+    {
+        score += points;
+        text.text = "Score: " + score;
+    }
+    public void ResetPoints()
+    {
+        score = 0;
+        text.text = "Score: " + score;
+    }
+}

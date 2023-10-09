1329511d4a4ae766f736dec80672481bcc1f0072
SUBERNER <80803238+SUBERNER@users.noreply.github.com>
2023-10-09 12:55:23 -0500

    Double jump and player movement
    Finished Player Movement:
    Smooth acceleration and deacceleration
    Variable jumping
    Sprinting
    
    All changes are inside object "Player 2.0"
    
    Added early collectable temp object for future powerups
    Added early double jump powerup
    (player wont lose powerup if hit by enemy)
    
    Added a level grid prefab for future level design and developing.

--- CampusRush/Assets/Scripts/Collectibles/CollectibleObject.cs
+++ CampusRush/Assets/Scripts/Collectibles/CollectibleObject.cs
@@ -2,6 +2,7 @@
 //Date: April 4, 2023
 //This is put onto the collectible prefab
 //to allow for collectibles to be used in game.
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class CollectibleObject : MonoBehaviour
@@ -12,8 +13,16 @@ public class CollectibleObject : MonoBehaviour
     private GameObject[] uiEle = new GameObject[5];
     private GameObject canvas;
 
+    public AnimationCurve floatCurve; //used to control the floating animation
+    private float curvePosition; //the current position of the X on the float curve
+    private Vector2 orgin; //the default location of the collectibles when starting a game
+
+
     private void Awake()
     {
+        //gets player current location
+        orgin = transform.position;
+
         SpriteRenderer sprite = GetComponent<SpriteRenderer>();
         sprite.sprite = item.icon;
         uiEle = GameObject.FindGameObjectsWithTag("UI");
@@ -24,14 +33,42 @@ public class CollectibleObject : MonoBehaviour
         }
     }
 
+    //used to give an collectable object a floating animation and leave it less static
+    private void Update()
+    {
+        //keeps number between 0 and 1
+        curvePosition += Time.deltaTime;
+        curvePosition %= 2f;
+
+        //animates pickup items to float in place
+        transform.position = new Vector2(transform.position.x, orgin.y + floatCurve.Evaluate(curvePosition));
+
+    }
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.gameObject.tag == "Player" && !collision.isTrigger)//Make sure the player has this tag, or the script won't work!
         {
             Debug.Log("Found the player!");
+            SendPowerUpData(item.id, collision); //sends data about poweup to player
             canvas.GetComponent<ScoreTracker>().AddPoints(score);
             CollectibleInventory.instance.AddCollectible(item);
             Destroy(gameObject);
+        }
+    }
+
+    //used to make changes to the player to when the player has been given a powerup
+    //use for future powerups
+    private void SendPowerUpData(int id, Collider2D collision)
+    {
+        switch(id) //the id of the powerup
+        {
+            case 2: //double jump powerup
+            {
+                collision.gameObject.GetComponent<PlayerMovement>().canDoubleJump = true;
+                break;
+            }
+
         }
     }
 }
